f352e374565c5d6fdbc293c7d696bc3f30c16909
jeremyd <jeremydeininger@gmail.com>
2013-03-31 20:11:59 -0700

    fixing package build
    Change-Id: I56e3dd4422afff6e4e5863f84578e0a18af30f0f

--- package_build/ubuntu/12.04/build.sh
+++ package_build/ubuntu/12.04/build.sh
@@ -5,17 +5,18 @@ DEBNAME=ebssense
 BUILDROOT=tmp/build_root
 
 mkdir -p tmp/build_root
+mkdir -p tmp/build_root/var/lib/clustersense
 
 sudo apt-get update
 sudo apt-get install -y git ruby1.9.1 build-essential ruby1.9.1-dev libxml2-dev libxslt-dev libsqlite3-dev lvm2 xfsprogs
 sudo gem install bundler --no-user --no-rdoc --no-ri
 bundle install --standalone
 
-rsync -rv lib/ $BUILDROOT/var/lib/clustersense/lib
-rsync -rv bin/ $BUILDROOT/var/lib/clustersense/bin
-rsync -rv bundle/ $BUILDROOT/var/lib/clustersense/bundle
-rsync -rv .bundle/ $BUILDROOT/var/lib/clustersense/.bundle
-rsync -rv spec/ $BUILDROOT/var/lib/clustersense/spec
+rsync -rv lib/ $BUILDROOT/var/lib/clustersense/lib/
+rsync -rv bin/ $BUILDROOT/var/lib/clustersense/bin/
+rsync -rv bundle/ $BUILDROOT/var/lib/clustersense/bundle/
+rsync -rv .bundle/ $BUILDROOT/var/lib/clustersense/.bundle/
+rsync -rv spec/ $BUILDROOT/var/lib/clustersense/spec/
 
 sudo gem install fpm --no-user --no-rdoc --no-ri
 

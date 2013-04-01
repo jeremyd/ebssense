e7cb92bebc7149bc15de788548bd310c173c48fc
jeremyd <jeremydeininger@gmail.com>
2013-03-31 20:46:46 -0700

    adding package hooks for bundler
    Change-Id: Ie7ba33eccdb0dc9d8886e4945df1a8923493b8d1

--- package_build/ubuntu/12.04/build.sh
+++ package_build/ubuntu/12.04/build.sh
@@ -6,6 +6,7 @@ BUILDROOT=tmp/build_root
 
 mkdir -p tmp/build_root
 mkdir -p tmp/build_root/var/lib/clustersense
+mkdir -p tmp/build_root/usr
 
 sudo apt-get update
 sudo apt-get install -y git ruby1.9.1 build-essential ruby1.9.1-dev libxml2-dev libxslt-dev libsqlite3-dev lvm2 xfsprogs
@@ -13,11 +14,11 @@ sudo gem install bundler --no-user --no-rdoc --no-ri
 bundle install --standalone
 
 rsync -rv lib/ $BUILDROOT/var/lib/clustersense/lib/
-rsync -rv bin/ $BUILDROOT/var/lib/clustersense/bin/
+rsync -rv bin/ $BUILDROOT/usr/bin/
 rsync -rv bundle/ $BUILDROOT/var/lib/clustersense/bundle/
 rsync -rv .bundle/ $BUILDROOT/var/lib/clustersense/.bundle/
 rsync -rv spec/ $BUILDROOT/var/lib/clustersense/spec/
 
 sudo gem install fpm --no-user --no-rdoc --no-ri
 
-fpm -s dir -t deb -n ebssense -v $EBSSENSE_VERSION -C $BUILDROOT -p $DEBNAME -d lvm2 -d xfsprogs -d ruby1.9.1 -d libxml2 -d libxslt1.1 var
+fpm -s dir -t deb -n ebssense -v $EBSSENSE_VERSION -C $BUILDROOT -p $DEBNAME -d lvm2 -d xfsprogs -d ruby1.9.1 -d libxml2 -d libxslt1.1 var usr
